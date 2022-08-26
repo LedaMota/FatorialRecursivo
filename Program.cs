fbe835b1a377c3a5e6c9fe9246139bf8ab9b35fb
Samuel Ribeiro Batista <leidymotastz@outlook.com>
2022-08-26 18:09:46 -0300

    Primeiro commit

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FatorialRecurcivo
+{
+    class Program
+    {
+        //Aqui e um metodo chamado main 
+        static void Main(string[] args)
+        {
+            Console.Write("Caucular de fatorial de qual o numero?");
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine("O fatorial de"+ num + "é" + Fatorial(num));
+
+            Console.ReadKey();
+        }
+
+            //Aqui é um metodo fatorial que chama se proprio e calcula o numero inteiro
+        static int Fatorial (int num)
+        { 
+          if (num <= 0)
+          {
+              return 1;
+          }
+                 //Aqui esta retornando o proprio valor do antecessor
+            return num * Fatorial(num - 1);
+            //5 * fatorial(4);
+            //4 * fatorial(3);
+            //3 * fatorial(2);
+            //2 * fatorial(1);
+            //1 * fatorial(0);
+            //return 1;
+        }
+
+
+    }
+
+
+}
